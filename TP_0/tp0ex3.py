02c61a80fa5b2d6cfac76fc976b906522d22af95
Alexis DRAI <drai.alexis@gmail.com>
2021-09-24 16:40:53 +0200

    Move into new repo

---
+++ TP_0/tp0ex3.py
@@ -0,0 +1,12 @@
+a = 10
+b = 12
+if a >= b:
+    print("a est plus grand")
+else:
+    print("b est plus grand")
+print("ceci s'affiche toujours")
+
+a = 10
+if a >= 9 and a <= 11:
+    print("a est dans l'intervalle [9, 11]")
+
