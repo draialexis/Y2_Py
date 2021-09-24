79d28a95c3aa445917319321dc0a1c8721695ee0
Alexis DRAI <drai.alexis@gmail.com>
2021-09-24 17:53:09 +0200

    Rewrite formula, preserve solution in a var

--- TP_1/tp1ex2.py
+++ TP_1/tp1ex2.py
@@ -1,9 +1,14 @@
 import numpy as np
+
+sol = 0
+
 n = 1
 isFound = False
+
 while not isFound:
-    if n > np.sqrt(2000000 - n):
-        print('solution is:',n)
+    if n*(n+1)/2 > 1000000:
+        sol = n
+        print('solution is:',sol)
         print('(', n, '(', n, '+', 1, ')', '/', 2 ,'=', (n*(n+1))/2)
         print('(', n-1, '(', n-1, '+', 1, ')', '/', 2 ,'=', (n*(n-1))/2)
         isFound = True
