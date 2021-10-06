40318656f2e085e6b1ef1f97caa9043a8c406c26
Alexis DRAI <aldrai>
2021-10-06 09:14:44 +0200

    Start ex1 tp3

---
+++ TP_3/tp3ex1.py
@@ -0,0 +1,73 @@
+#1)
+
+def code_une(lt):
+    switcher = {
+        'A': 0,
+        'B': 1,
+        'C': 2,
+        'D': 3,
+        'E': 4,
+        'F': 5,
+        'G': 6,
+        'H': 7,
+        'I': 8,
+        'J': 9,
+        'K': 10,
+        'L': 11,
+        'M': 12,
+        'N': 13,
+        'O': 14,
+        'P': 15,
+        'Q': 16,
+        'R': 17,
+        'S': 18,
+        'T': 19,
+        'U': 20,
+        'V': 21,
+        'W': 22,
+        'X': 23,
+        'Y': 24,
+        'Z': 25,
+    }
+    return switcher.get(lt, "entrez seulement des lettres majuscules sans accent.")
+
+str = "CESAR"
+for lt in str:
+    print(code_une(lt))
+print('\n')
+#2)
+
+def decode_une(c):
+    switcher = {
+        0: 'A',
+        1: 'B',
+        2: 'C',
+        3: 'D',
+        4: 'E',
+        5: 'F',
+        6: 'G',
+        7: 'H',
+        8: 'I',
+        9: 'J',
+        10: 'K',
+        11: 'L',
+        12: 'M',
+        13: 'N',
+        14: 'O',
+        15: 'P',
+        16: 'Q',
+        17: 'R',
+        18: 'S',
+        19: 'T',
+        20: 'U',
+        21: 'V',
+        22: 'W',
+        23: 'X',
+        24: 'Y',
+        25: 'Z',
+    }
+    return switcher.get(c, "entrez seulement des chiffresentre 0 et 25.")
+
+code = [2, 4, 18, 0, 17]
+for c in code:
+    print(decode_une(c))
