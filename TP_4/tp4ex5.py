e8bd9291a77ec10ce9fef0c80f62aee34ef387c8
Alexis DRAI <aldrai>
2021-10-20 09:02:06 +0200

    Do ex 5, non functionny curves

---
+++ TP_4/tp4ex5.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+t = np.linspace(0, 2 * np.pi)
+x = np.cos(t)
+y = np.sin(t)
+plt.axis('equal')
+plt.plot(x, y)
+plt.plot([-0.866, 0.866, 0, -0.866], [-0.5, -0.5, 1, -0.5])
+plt.show()
