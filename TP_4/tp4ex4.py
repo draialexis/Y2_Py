7ff023c8ee5db78e394921454408580590822bae
Alexis DRAI <aldrai>
2021-10-20 08:42:17 +0200

    Do ex 4, relearn how to find a tangent

---
+++ TP_4/tp4ex4.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+my_x = np.linspace(0, 10, 300)
+my_y = 1/my_x
+
+def y_tan(x0, x) :
+    return  (-1 / (x0**2)) * (x - x0) + (1 / x0)
+
+my_y_t = y_tan(2, my_x)
+
+plt.plot(my_x, my_y)
+plt.plot(my_x, my_y_t)
+plt.axis('equal')
+plt.show()
