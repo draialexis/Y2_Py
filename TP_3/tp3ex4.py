9b985be0719df1a836a821c2084adaa41c6826be
Alexis DRAI <aldrai>
2021-10-27 08:01:16 +0200

    Do ex4 tp3, recursion and euler's number

---
+++ TP_3/tp3ex4.py
@@ -0,0 +1,20 @@
+def factorial(n):
+    if n == 1:
+        return 1
+    elif n > 1:
+        return n * factorial (n-1)
+    else:
+        print("Nnnno no no")
+
+def euler(n):
+    if n == 0:
+        return 1
+    elif n > 0:
+#        print('factorial(',n , ') =', factorial(n))
+        return euler(n-1) + 1 / (factorial(n))
+    else:
+        print("that's a woopsie...'")
+        return
+
+my_n = 13
+print('euler(', my_n, ') =', euler(my_n))
