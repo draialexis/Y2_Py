3bde5da3501960c6a1d92e6c6ae1846af705406c
Alexis DRAI <drai.alexis@gmail.com>
2021-11-04 14:29:54 +0100

    Add legends and titles

--- TP_4/tp4ex3.py
+++ TP_4/tp4ex3.py
@@ -1,13 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 1)
+# 1) & 2)
 
 t = np.arange(0.3, 2, 0.2)
 x = np.linspace(0, 1, 100)
 for i in range(len(t)):
     y = pow(x, t[i])
-    plt.plot(x, y)
+    my_str = "x^" + str(round(t[i], 2))
+    plt.plot(x, y, label=my_str)
+plt.legend(loc=0)
+plt.title("f_t: x -> x^t, x€[0,1]")
+plt.axis('equal')
+plt.grid()
+plt.figure()
+
+# 3)
+
+t_ = np.arange(0.4, 3, 0.1)
+x_ = np.linspace(0, 1, 100)
+for i in range(len(t_)):
+    y_ = pow(x_, t_[i])
+    my_str = "x^" + str(round(t_[i], 2))
+    plt.plot(x_, y_)
+plt.title("f_t: x -> x^t, x€[0,1]")
 plt.axis('equal')
 plt.grid()
 plt.show()
