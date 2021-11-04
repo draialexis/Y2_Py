5497e327b916a02d884dd1a86ad8887c6b0b228b
Alexis DRAI <drai.alexis@gmail.com>
2021-11-04 10:29:42 +0100

    Improve Farey's displays

--- TP_2/tp2ex6.py
+++ TP_2/tp2ex6.py
@@ -20,7 +20,7 @@ def phi(n):
     count = 0
     for i in range(1, n + 1):
         if pgcd(i, n) == 1:
-            # print(i,'/',n)
+            # print(i, '/', n)
             count += 1
     return count
 
@@ -35,13 +35,15 @@ def farey(n):
     if n == 0:
         print('no no no no no')
         return -1
-    count = 0
-    for i in range(0, n + 1):
+    count = 1
+    # print('0 / 1')
+    for i in range(1, n + 1):
         for j in range(1, n + 1):
-            if pgcd(i, j) == 1 and i <= j:
-                #                print(i,'/',j)
+            if pgcd(i, j) == 1 and i < j:
+                # print(i, '/', j)
                 count += 1
-
+    # print('1 / 1')
+    count += 1
     return count
 
 
