54b844c915fbad429074e2c342e02a0c794f9c63
Alexis DRAI <drai.alexis@gmail.com>
2021-11-05 14:50:09 +0100

    Do weird integrations, fix bug in tp2ex2

---
+++ TP_5/tp5ex7.py
@@ -0,0 +1,36 @@
+import sympy as sm
+from sympy import sqrt, pprint, diff, integrate, log, simplify, factor
+
+sm.var('x n a')
+
+# 1)
+
+f = (x ** (n - 1)) / (a + x)
+print('f =')
+pprint(f)
+v = integrate(f, (x, 0, 1))
+print('\ninteg(f[0,1]) =')
+pprint(v)
+
+# integ(f[0,1]) = ...wat.
+
+# 2)
+
+f = f.subs(n, 40)
+print('f =')
+pprint(f)
+v = integrate(f, (x, 0, 1))
+print('\ninteg(f[0,1], n:40) =')
+pprint(v)
+
+# 3)
+
+v = v.subs(a, 3)
+print('\ninteg(f[0,1], n:40, a:3) =')
+pprint(v)
+
+# 4)
+
+print(v.evalf(20))
+
+# our result from tp2ex2 was pretty close, this is better obviously
