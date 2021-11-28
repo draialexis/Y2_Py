5b857894616a733f4593c195b4fbf330cd195f47
Alexis DRAI <drai.alexis@gmail.com>
2021-11-28 14:28:01 +0100

    Do 'drunk walk' tp7ex5 1 and 2

--- TP_7/tp7ex5.py
+++ TP_7/tp7ex5.py
@@ -4,17 +4,37 @@ from numpy.random import rand
 
 print('########1########')
 
+s = 5000
+
 
 def bern(a, b, p):
-    r = rand()
-    if r <= p:
+    if rand() <= p:
         return b
     else:
         return a
 
 
-steps = [(bern(-1, 1, 0.5)) for _ in range(10)]
+steps = [(bern(-1, 1, 0.5)) for _ in range(s)]
 walk = np.cumsum(steps)
-print(steps)
-print(walk)
-# plt.scatter()
+x = np.linspace(0, s, s)
+plt.plot(x, walk)
+plt.ylabel("left to right")
+plt.xlabel("0 to r")
+plt.title('drunk walk, from (0, 0) to (r, 0)')
+plt.figure()
+
+print('########2########')
+
+r = 500
+s = 500
+
+for i in range(r):
+    steps = [(bern(-1, 1, 0.5)) for _ in range(s)]
+    walk = np.cumsum(steps)
+    x = np.linspace(0, s, s)
+    plt.plot(x, walk)
+
+plt.ylabel("left to right")
+plt.xlabel("0 to r")
+plt.title('500 drunk walks, from (0, 0) to (r, 0)')
+plt.show()
