384c0e80699e996804309b7503c82a0bf60b56a3
Alexis DRAI <drai.alexis@gmail.com>
2021-11-28 13:56:01 +0100

    Touch up tp7ex4

--- TP_7/tp7ex4.py
+++ TP_7/tp7ex4.py
@@ -5,11 +5,11 @@ print('########1########')
 
 
 def serie(n):
-    L = randint(1, 7, n)
+    L = randint(1, 7, size=n)
     return L
 
 
-print(serie(100))
+print(serie(10))
 
 print('########2########')
 
@@ -20,26 +20,34 @@ tally = {
     'no': 0
 }
 
-"""
-1/6 chances ; *4 => 4/6 chances (66.6%)
-hy do I get 51.7% (4/7...)
-
-"""
-
-r = 200
+r = 20000
 
 for i in range(r):
-    if 6 in serie(4):
-        tally['yes'] += 1
-    else:
+    nums = serie(4)
+    found_one = 0
+    for num in nums:
+        if num == 6:
+            tally['yes'] += 1
+            found_one = 1
+            break
+    if found_one == 0:
         tally['no'] += 1
 
-tally['yes'] = tally['yes'] * 100 / r
-tally['no'] = tally['no'] * 100 / r
+y = tally['yes']
+n = tally['no']
+tally['yes'] = y * 100 / (y + n)
+tally['no'] = n * 100 / (y + n)
 
 print(tally)
 plt.bar(tally.keys(), tally.values(), color='g')
-plt.figure()
+plt.show()
+
+"""
+1/6 chances ; *4 => 2/3 chances (66.7%)
+why do I get 51.7% (4/7...)?
+probably linked to the fact that we look for 'at least' one 6:
+we're not looking at all the throws, we're skipping some 6s
+"""
 
 print('############b####')
 
@@ -57,9 +65,7 @@ for _ in range(r):
     for i, item in enumerate(A):
         if item == 6 and B[i] == 6:
             found_one = 1
-            # print('A[', i, '] = B[', i, '] = ', item)
             tally['yes'] += 1
-            # break;
     if found_one == 0:
         tally['no'] += 1
 
@@ -67,5 +73,15 @@ tally['yes'] = tally['yes'] * 100 / r
 tally['no'] = tally['no'] * 100 / r
 
 print(tally)
-plt.bar(tally.keys(), tally.values(), color='g')
+plt.bar(tally.keys(), tally.values(), color='b')
 plt.show()
+
+"""
+1/36 chances (2.7%) ; *24 => 2/3 chances (66.7%)
+we can get 66.7% if we count every time a double 6 occurs.
+but if we do that and we don't count every time it does not occur, 
+the total is above 100%. The instructions don't say 'at least'...
+If we do compare 'yes' to 'no' for every double throw, then doing series of 24
+is pointless, and we can just do one series of 20000 or so.
+We will find 2.7% likelihood.
+"""
