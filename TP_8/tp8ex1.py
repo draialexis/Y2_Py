277f94eda6a0c70da947d6ea4d7851a8a8383eca
Alexis Drai <drai.alexis@gmail.com>
2021-12-01 09:08:57 +0100

    Begin TP8, use arrays to examine sin()

---
+++ TP_8/tp8ex1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.hist(np.sin(np.arange(0, 10000, 1)), bins=np.linspace(-1, 1, 100), edgecolor='black')
+plt.show()
+
+"""
+hist des 10000 1er val de la suite, comparer les barres
+"""
+
+print("the terms given by sin(x) don't appear to be uniformly spaced within the [-1, 1] interval")
